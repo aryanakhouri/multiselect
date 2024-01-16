852fdb694135b8000e3c13f307920c969bd071fc
whyaryanakhouri <aryan.akhouri@cogoport.com>
2024-01-17 00:29:01 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,1 @@
-import './App.css';
-import MultiSelect from './components/MultiSelect';
 
-function App() {
-  return (
-    <div className="App">
- <MultiSelect/>
-    </div>
-  );
-}
-
-export default App;
